e86918637b4b29e7eb4145e4fb3b4ca8bc6bc425
sverdiyev <79407224+sverdiyev@users.noreply.github.com>
2023-10-29 13:16:48 +0100

    fix dummy controller

--- src/main/java/com/boiz/employeedashboardbe/employees/EmployeesController.java
+++ src/main/java/com/boiz/employeedashboardbe/employees/EmployeesController.java
@@ -5,9 +5,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("employees")
+@RestController
+@RequestMapping("employees")
 public class EmployeesController {
 
   @GetMapping
