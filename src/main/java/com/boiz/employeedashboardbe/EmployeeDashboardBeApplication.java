a07f9534319339213ec0c226f2daf8d527cecc4b
sverdiyev <79407224+sverdiyev@users.noreply.github.com>
2023-10-29 12:45:37 +0100

    initial commit

---
+++ src/main/java/com/boiz/employeedashboardbe/EmployeeDashboardBeApplication.java
@@ -0,0 +1,13 @@
+package com.boiz.employeedashboardbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDashboardBeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EmployeeDashboardBeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/boiz/employeedashboardbe/EmployeeDashboardBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boiz.employeedashboardbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeDashboardBeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
