c2a45867bc0d155a7cb119e3ff87f599d4ebc3d7
sverdiyev <79407224+sverdiyev@users.noreply.github.com>
2023-10-29 13:40:18 +0100

    generate hibernate dlls

--- src/main/java/com/boiz/employeedashboardbe/employees/model/Employee.java
+++ src/main/java/com/boiz/employeedashboardbe/employees/model/Employee.java
@@ -1,5 +1,9 @@
 package com.boiz.employeedashboardbe.employees.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -10,8 +14,11 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Employee {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
   private String name;
   private String address;
